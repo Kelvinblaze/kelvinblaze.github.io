0d655370b5934919a9ffa13ebe03892bcc22bb59
Ifeanyi Kelvin Ossai <ifeanyiko4u@gmail.com>
2019-09-19 20:54:19 +0100

    lasudebate

--- lasudebate/assets/js/main.js
+++ lasudebate/assets/js/main.js
@@ -7,18 +7,22 @@ menuBar.addEventListener("click", e => {
 });
 
 // DIALOGUE
-var modal = document.querySelector("#form-modal");
+var htmlDoc = document.all[0];
+var body = document.body;
+var modal = document.querySelector(".form-modal");
 var closeModal = document.querySelector(".closeModal");
 var modalBtn = document.querySelector(".modalBtn");
 
 modalBtn.addEventListener("click", e => {
   e.preventDefault();
-  modal.showModal();
-  document.body.style.overflow = "hidden";
+  modal.style.display = "block";
+  body.style.overflow = "hidden";
+  htmlDoc.classList = "backdrop";
 });
 
 closeModal.addEventListener("click", e => {
   e.preventDefault();
-  modal.close();
-  document.body.style.overflow = "auto";
+  modal.style.display = "none";
+  body.style.overflow = "scroll";
+  htmlDoc.classList = "rmvbackdrop";
 });
