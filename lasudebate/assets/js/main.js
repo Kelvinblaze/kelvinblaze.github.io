9b2d072a137d3e140635efe4d5065176dc472123
Ifeanyi Kelvin Ossai <ifeanyiko4u@gmail.com>
2019-10-05 07:23:12 +0100

    lasu debate

--- lasudebate/assets/js/main.js
+++ lasudebate/assets/js/main.js
@@ -1,5 +1,7 @@
 var menuBar = document.querySelector("#bars");
 var menuNavBar = document.querySelector(".mobile-list");
+var form = document.querySelector("#modalform");
+let submitBtn = document.querySelector(".submitBtn");
 
 menuBar.addEventListener("click", e => {
   e.preventDefault();
@@ -23,6 +25,80 @@ modalBtn.addEventListener("click", e => {
 closeModal.addEventListener("click", e => {
   e.preventDefault();
   modal.style.display = "none";
-  body.style.overflow = "scroll";
+  body.style.overflowY = "scroll";
   htmlDoc.classList = "rmvbackdrop";
 });
+// Loader
+let loaderDiv = document.querySelector(".loader");
+loaderDiv.style.display = "none";
+
+// Show loader function
+
+function showLoader() {
+  loaderDiv.style.display = "block";
+}
+// Hide Loader
+function hideLoader() {
+  loaderDiv.style.display = "none";
+}
+// Close Modal function
+
+function closeformModal() {
+  modal.style.display = "none";
+  body.style.overflowY = "scroll";
+  htmlDoc.classList = "rmvbackdrop";
+}
+
+//  getting data
+db.collection("student-details")
+  .get()
+  .then(snapshot => {
+    snapshot.docs.forEach(doc => {
+      console.log(doc.data());
+    });
+  });
+
+// Add data to firebase
+submitBtn.onclick = e => {
+  e.preventDefault();
+
+  // Form inputs
+  let fullname = document.querySelector("#fullname").value;
+  let uniName = document.querySelector("#uni_name").value;
+  let uniAddress = document.querySelector("#uni_address").value;
+  let email = document.querySelector("#email").value;
+  let phoneNumber = document.querySelector("#phone_number").value;
+
+  if (
+    fullname != "" &&
+    uniName != "" &&
+    uniAddress != "" &&
+    email != "" &&
+    phoneNumber != ""
+  ) {
+    showLoader();
+    db.collection("student-details")
+      .add({
+        fullname: fullname,
+        university_name: uniName,
+        university_address: uniAddress,
+        email: email,
+        phone_number: phoneNumber
+      })
+      .then(docRef => {
+        if (docRef.id) {
+          hideLoader();
+          alert("Thank You, Your details has been submitted successfully");
+          closeformModal();
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        alert("An error occured. Please Try Again");
+      });
+
+    // closeformModal();
+  }
+};
+
+function sendData() {}
